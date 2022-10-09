95ea4e22adf9ec95bcd6f728b6c6efef221c28a7
Eric Avila <arnoldavila2001@gmail.com>
2022-10-09 19:38:03 -0400

    adding commands

---
+++ AppendCommand.java
@@ -0,0 +1,16 @@
+package team_design_pattern;
+
+import java.util.Scanner;
+
+public class ViewCommand{
+
+    private Scanner in;
+
+    public AppendCommand(Document doc){
+
+    }
+
+    public String execute(){
+        return "";
+    }
+}
---
+++ SaveCommand.java
@@ -0,0 +1,13 @@
+package team_design_pattern;
+
+
+public class SaveCommand{
+
+    public SaveCommand(Document doc){
+
+    }
+
+    public String execute(){
+        return "";
+    }
+}
