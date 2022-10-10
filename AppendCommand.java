5cfece63de31357b2949a6b8ea9649569a9443c8
Eric Avila <arnoldavila2001@gmail.com>
2022-10-09 20:09:15 -0400

    Eric commit 2

--- AppendCommand.java
+++ AppendCommand.java
@@ -9,9 +9,15 @@ public class ViewCommand{
     public AppendCommand(Document doc){
 
         in = new Scanner(System.in);
+        this.doc = doc;
+
     }
 
     public String execute(){
-        return "";
+
+        System.out.println("What do you want to append to the list?");
+        String appendString = in.nextLine();
+        document.append(appendString);
+        return "The line has been appended to the document";
     }
 }
