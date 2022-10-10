89b92136c02c1e88dedefbaafb07dc63c97683b5
jiangrick6 <rjiang@email.sc.edu>
2022-10-09 20:29:35 -0400

    Rick commit 2

---
+++ InputHandler.java
@@ -0,0 +1,25 @@
+package team_design_pattern;
+ 
+import java.util.HashMap;
+ 
+public class InputHandler{
+    private HashMap<String, Command> commands;
+ 
+ 
+    public InputHandler(Document document){
+        commands = new HashMap<String, Command>();
+        commands.put("view", new ViewCommand(document));
+        commands.put("Append", new AppendCommand(document));
+        commands.put("Write", new WriteCommand(document));
+        commands.put("Save",new SaveCommand(document));
+ 
+    }
+    public void inputEntered(String data){
+        if(commands.containsKey(data)){
+            commands.get(data).execute();
+        }
+    }
+ 
+ 
+}
+
