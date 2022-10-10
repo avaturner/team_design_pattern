e0dd995e6f1ecdbc65beb61d636781cef5119cae
mark <shperkin@email.sc.edu>
2022-10-09 21:22:37 -0400

    fixed package

--- Command.java
+++ Command.java
@@ -1,15 +1,26 @@
 package team_design_pattern;
 
 import org.w3c.dom.Document;
-
+/**
+ * @author Mark Shperkin
+ * this class constructs the commands
+ */
 public abstract class Command {
-    
+    /**
+     * creates a Document and calls it document
+     */
     protected Document document;
-
+/**
+ * consructs a document
+ * @param document takes a document in and assigns it to document
+ */
     public Command(Document document) {
         this.document = document;
     }
-
+/**
+ * this method calls the commands execute methods
+ * @return returns a 
+ */
     public String execute() {
         return this.execute();
     }
